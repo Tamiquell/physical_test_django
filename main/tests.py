8de9bef0966d00ed531269798d9c880c6fad8913
Tamiquell <tamir2013@inbox.ru>
2022-05-29 22:01:12 +0300

    add tests

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from .forms import UserDateForm
+
+
+class FormTests(TestCase):
+    def test_forms(self):
+        form_data = {'date': datetime.date.today()}
+        form = UserDateForm(data=form_data)
+        self.assertTrue(form.is_valid())
